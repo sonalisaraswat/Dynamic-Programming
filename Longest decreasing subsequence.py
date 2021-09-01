a8bf09a919d1239ad7fba1f30deaf571de9b3ec0
Sonali Saraswat <35163421+sonalisaraswat@users.noreply.github.com>
2021-09-01 15:55:17 +0530

    Update Longest decreasing subsequence.py

--- Longest decreasing subsequence.py
+++ Longest decreasing subsequence.py
@@ -1,8 +1,8 @@
 def Lds(arr, n):
     lds = [1]*n
-    for i in range(n):
-        for j in range(i):
-            if(arr[i] < arr[j] and lds[i] < lds[j]+1):
+    for i in range(n):    # i goes from 0 to n-1
+        for j in range(i):   # j goes from 0 to i-1
+            if(arr[j] > arr[i] and lds[i] < lds[j]+1):
                 lds[i] = lds[j]+1
     return max(lds)
 arr = [int(x) for x in input("Input the array to find the longest decreasing subsequence: ").split(" ")]
