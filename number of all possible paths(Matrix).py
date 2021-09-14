dcb374428d117fb99ce99fdc8676491ea0e239b2
Sonali Saraswat <35163421+sonalisaraswat@users.noreply.github.com>
2021-09-14 11:44:30 +0530

    Update number of all possible paths(Matrix).py

--- number of all possible paths(Matrix).py
+++ number of all possible paths(Matrix).py
@@ -1,1 +1,8 @@
-
+print("Enter the dimension: ")
+N = int(input())
+M = int(input())
+def NumPath(N,M):
+  if(N == 1 or M == 1):
+    return 1
+  return NumPath(N-1, M) + NumPath(N, M-1)
+print(NumPath(N,M))
