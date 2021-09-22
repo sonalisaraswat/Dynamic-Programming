7828306351111c5b1f1b7e9ebb105b6df04ffad0
Sonali Saraswat <35163421+sonalisaraswat@users.noreply.github.com>
2021-09-22 14:52:23 +0530

    Create Can big str using small strs.py

---
+++ Can big str using small strs.py
@@ -0,0 +1,31 @@
+def CanStr(TargetStr, SmallStrs, Table):
+
+    if TargetStr in Table:
+        return Table[TargetStr]
+
+    if TargetStr == '':                                                 # no suffix left to check all prefix matched, so return True.
+        return True
+
+    for strg in SmallStrs:
+        if TargetStr.startswith(strg):                                   # if any string is the prefix of target, remove it and check for rest of suffix.
+            suffix = TargetStr[len(strg):]
+            if CanStr(suffix, SmallStrs, Table) == True:                 # CanStr() only return True / False.
+                Table[suffix] = True
+                return True
+
+    Table[TargetStr] = False
+    return False
+
+
+# Start = find if Target string can be formed from given small strings
+TargetStr = 'abcdef'
+SmallStrs = ['abc', 'de', 'ab', 'cde','f']
+Table = {}
+print(CanStr(TargetStr, SmallStrs, Table))                                #True
+print(Table)                                                              #{'': True, 'f': True, 'def': True}
+
+
+# WORKING:  abcdef = abc + def (matched prefix; now recur for suffix )
+#           def    = de + f    (matched prefix; now recur for suffix )
+#           f      = f + ''    (matched prefix; now recur for suffix )
+#           '' returns True
