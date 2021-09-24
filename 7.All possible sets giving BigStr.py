1486c36902fa3ba90bd32f80f703e7902440f98a
Sonali Saraswat <35163421+sonalisaraswat@users.noreply.github.com>
2021-09-25 00:25:14 +0530

    Update 7.All possible sets giving BigStr.py

--- 7.All possible sets giving BigStr.py
+++ 7.All possible sets giving BigStr.py
@@ -1,34 +1,26 @@
-def allCon(Target, Arr, Table):
-    if Target in Table:
-        return Table[Target]
-
+def allCon(Target, Arr):
     if Target == '':
         return [[]]
-
     B = None
-
     for strg in Arr:
         if Target.startswith(strg):
             suf = Target[len(strg):]
-            val = allCon(suf,Arr,Table)
+            val = allCon(suf,Arr)
             if val is not None:
                 for row in val:
                     row.insert(0, strg)
                 if B is None:
                     B = val[:][:]
                 else:
-                    B.append(val[:][:])
-                print(B)
-                if Target == fin:
-                    allPossible.append(B[:][:])
-    h = str(B)
-    Table[h] = B
+                    for row in val:
+                        B.append(row)
+
     return B
 Target = 'abcdef'
 Arr = ['ab','cd','e','f','ef','a','b']
-fin = Target
-allPossible = []
-Table = {}
-allCon(Target, Arr,Table)
-print(allPossible[:][:])
-print(Table)
+print(allCon(Target, Arr))
+
+# OUTPUT: [['ab', 'cd', 'e', 'f'], 
+#          ['ab', 'cd', 'ef'], 
+#          ['a', 'b', 'cd', 'e', 'f'], 
+#          ['a', 'b', 'cd', 'ef']]
