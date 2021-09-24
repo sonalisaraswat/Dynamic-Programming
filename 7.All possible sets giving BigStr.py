490fefa99107ce61fcbaf1c81d8e61431553b1ec
Sonali Saraswat <35163421+sonalisaraswat@users.noreply.github.com>
2021-09-24 15:45:05 +0530

    Update 7.All possible sets giving BigStr.py

--- 7.All possible sets giving BigStr.py
+++ 7.All possible sets giving BigStr.py
@@ -1,41 +1,34 @@
-# We can use elements of Array unlimited times.
-
-
 def allCon(Target, Arr, Table):
-
     if Target in Table:
         return Table[Target]
 
     if Target == '':
-        return ''
+        return [[]]
 
     B = None
 
-    for a in Arr:
-        if Target.startswith(a):
-            suf = Target[len(a):]
-            val = allCon(suf,Arr, Table)                # Returns either NONE or comma seperated elements(as single string)
-            
+    for strg in Arr:
+        if Target.startswith(strg):
+            suf = Target[len(strg):]
+            val = allCon(suf,Arr,Table)
             if val is not None:
-                B = a+','+val
-                
-            if(Target == fin) and B is not None:       # place set only if parent is reached
-                allPossible.append( B[:-1].split(',') )
-
-
-    Table[Target] = B
+                for row in val:
+                    row.insert(0, strg)
+                if B is None:
+                    B = val[:][:]
+                else:
+                    B.append(val[:][:])
+                print(B)
+                if Target == fin:
+                    allPossible.append(B[:][:])
+    h = str(B)
+    Table[h] = B
     return B
-
-
-# MAIN START
-
 Target = 'abcdef'
-Arr = ['abc','a','d','bcd','ef']
+Arr = ['ab','cd','e','f','ef','a','b']
 fin = Target
-allPossible = []                                       # Array for storing; array of all possible sets making Target string.
-Table = {}                                             # For MEMO
-allCon(Target, Arr,Table)                              # Calling
-print(allPossible)                                     # all sets are placed in 2D array T
-
-# OUTPUT:
-# [['abc', 'd', 'ef'], ['a', 'bcd', 'ef']]
+allPossible = []
+Table = {}
+allCon(Target, Arr,Table)
+print(allPossible[:][:])
+print(Table)
